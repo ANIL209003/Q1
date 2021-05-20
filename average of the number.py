ae0a42241549e9764c6823d9ff76d9e7c4b55192
ANIL KUMAR <71810304+ANIL209003@users.noreply.github.com>
2021-05-20 15:30:22 +0530

    Add files via upload

---
+++ average of the number.py
@@ -0,0 +1,9 @@
+math = int(input("enter math "))
+english = int(input("enter english "))
+science = int(input("enter science "))
+hindi = int(input("enter hindi "))
+pc = int(input("enter pc "))
+avg = ((math+english+science+hindi+pc)/5);
+percent = avg
+print ("average of 5 number is", avg)
+print ("percentage of 5 number is ", percent , "%")
